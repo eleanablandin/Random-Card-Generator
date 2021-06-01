8c4adfcc82bc306b7ca413f6f3549a0eec6393d6
eleanablandin <eleanablandin@gmail.com>
2021-06-01 14:34:34 -0400

    codigo random carad generator utilizando DOM

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomSymbol = () => {
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let azarSymbol = symbol[Math.round(Math.random() * (symbol.length - 1))];
+  return azarSymbol;
+};
+
+let randomNumber = () => {
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+  let azarNumbers = numbers[Math.round(Math.random() * (numbers.length - 1))];
+  return azarNumbers;
+};
+window.onload = () => {
+  let number = document.querySelector(".centerNumber");
+  number.textContent = randomNumber();
+
+  let symbol = randomSymbol();
+
+  let top = document.querySelector(".topSymbol");
+  top.textContent = symbol;
+
+  let bottom = document.querySelector(".bottomSymbol");
+  bottom.textContent = symbol;
 };
